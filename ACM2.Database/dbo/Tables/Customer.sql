56f3aed7a81fc6bb72c5faae37ec7c828eb9dcc1
izethewise <block.inlays@gmail.com>
2017-09-04 19:37:01 +0100

    Initial commit after importing DB into database project

---
+++ ACM2.Database/dbo/Tables/Customer.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Customer] (
+    [CustomerId]   INT           IDENTITY (1, 1) NOT NULL,
+    [LastName]     NVARCHAR (20) NULL,
+    [FirstName]    NVARCHAR (20) NULL,
+    [EmailAddress] NVARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([CustomerId] ASC),
+    CONSTRAINT [CK_Customer_Lastaname_Not_Empty] CHECK (len(rtrim([lastname]))>(0))
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_CustomerLastname]
+    ON [dbo].[Customer]([LastName] ASC);
+
